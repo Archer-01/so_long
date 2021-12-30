4a29407b50aa08eec4d740062f521e367bef013e
Hamza Haddani <hhamza@student.1337.ma>
2021-12-30 17:29:17 +0100

    Added get_next_line header (include/get_next_line.h)

---
+++ include/get_next_line.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/19 13:23:18 by hhamza            #+#    #+#             */
+/*   Updated: 2021/12/29 20:00:02 by hhamza           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFFER_SIZE 42
+
+// Utils
+size_t	ft_strlen(const char *str);
+char	*ft_strdup(const char *s1);
+int		ft_indexof(const char *str, char c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(const char *s1, const char *s2);
+
+// Main functions
+char	*get_next_line(int fd);
+char	*ft_save_remainder(char *line, char *remainder);
+
+#endif
