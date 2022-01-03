00dd56d7dc2f25494eb826e07708e48fd08eafd8
Hamza Haddani <hhamza@student.1337.ma>
2022-01-03 15:41:30 +0100

    Modified linked list line storing (Stored lines without '\n')

--- src/ft_map_parser.c
+++ src/ft_map_parser.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 10:36:52 by hhamza            #+#    #+#             */
-/*   Updated: 2022/01/03 13:35:27 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/01/03 15:34:28 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,17 @@ static t_list	*ft_map_to_list(int fd)
 	t_list	*lst;
 	t_list	*newnode;
 	char	*line;
+	char	*line_no_endl;
 
 	lst = NULL;
 	line = get_next_line(fd);
 	while (line != NULL)
 	{
-		newnode = ft_lstnew(line);
+		line_no_endl = ft_substr(line, 0, ft_indexof(line, '\n'));
+		free(line);
+		if (line_no_endl == NULL)
+			return (ft_lstclear(&lst, &free), NULL);
+		newnode = ft_lstnew(line_no_endl);
 		if (newnode == NULL)
 		{
 			ft_lstclear(&lst, &free);
@@ -59,6 +64,10 @@ t_list	*ft_map_parser(const char *path)
 		exit(2);
 	}
 	lst = ft_map_to_list(fd);
+	if (ft_check_rectangular(lst) == 1)
+		printf("Rectangular map\n");
+	else
+		printf("Non rectangular map\n");
 	close(fd);
 	return (lst);
 }
