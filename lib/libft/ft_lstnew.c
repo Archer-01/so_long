273f5cf33779e32e24f0f5c839d6ec8f19d79f60
Hamza Haddani <hhamza@student.1337.ma>
2022-01-03 11:01:03 +0100

    Updated ft_lstnew's Doxygen documentation

--- lib/libft/ft_lstnew.c
+++ lib/libft/ft_lstnew.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstnew_bonus.c                                  :+:      :+:    :+:   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hhamza <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:47:56 by hhamza            #+#    #+#             */
-/*   Updated: 2021/12/31 12:55:33 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/01/03 11:00:12 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
  * @brief Creates a new node
  *
  * @param content: content of the node
- * @return t_list*: memory address of the new node
+ * @return t_list*: memory address of the new node, NULL on failure
  */
 t_list	*ft_lstnew(void *content)
 {
